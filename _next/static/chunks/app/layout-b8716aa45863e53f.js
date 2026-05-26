2f16a84c9bb2f9e0ab873dea2abfc7cabce4543e
DaScient <DaScient@users.noreply.github.com>
2026-05-26 23:09:49 +0000

    deploy: 48ebb2457dfdc228f303f983f15de112e5841315

---
+++ _next/static/chunks/app/layout-b8716aa45863e53f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4202:function(n,e,u){Promise.resolve().then(u.t.bind(u,8877,23))},8877:function(){}},function(n){n.O(0,[404,971,23,744],function(){return n(n.s=4202)}),_N_E=n.O()}]);
